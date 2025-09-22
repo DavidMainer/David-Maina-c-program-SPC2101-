c6fa4a6d1f523acc6fa5e0d86fb50d1f9a7692cd
David Mainer <mainermaina2496@gmail.com>
2025-09-22 17:43:06 +0300

    Add files via upload
    uploaded assignment on naming semester one units

---
+++ units naming.c
@@ -0,0 +1,27 @@
+//program to desplay semester 1 units
+/*
+Name: David Mainer
+Reg no:PA106/G/28779/25
+*/
+
+#include<stdio.h>
+//Displaying the units
+ int main () {
+
+    printf("1. (SPM 2100):Discrete Mathematics\n\n");
+
+    printf("2. (SPM 2104):Mathematics for Science\n\n");
+
+    printf("3. (UCU 2105):Fundamentals of ICT\n\n");
+
+    printf("4. (SPI 2102):Computer Systems and Organization\n\n");
+
+    printf("5. (UCU 2101):Communication Skills\n\n");
+
+    printf("6. (SPC 2101):Introduction to Programming & Problem Solving\n\n");
+
+    printf("7. (SSE 2101):Fundamentals of System Analysis & Design\n\n");
+
+
+       return 0;
+       }
