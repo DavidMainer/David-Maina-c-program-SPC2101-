89b95d759fbc09fc318d96dfd221e62e5828c034
David Mainer <mainermaina2496@gmail.com>
2025-10-10 06:12:12 +0300

    uploaded assignment
    program using for loop to print 100-50 in descending order each number on its own line

---
+++ for loop.c
@@ -0,0 +1,20 @@
+// Description: A program using for loop to countdown from 100-50.
+
+// Name: David Maina Onchoke
+// Reg No.:PA106/G/28779/25
+
+
+#include <stdio.h>
+
+int main(){
+	 
+	int i;
+	
+//for loop to countdown from 100-50
+for(i=100;i>=50;i--){
+	
+	printf("%d\n",i);
+}
+	
+	return 0;
+}
